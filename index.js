ab38d045b95f406d70968475719c79978505085e
Anibal <androso421@gmail.com>
2021-12-22 16:08:05 -0600

    adding routes for the most basic paths

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const App = express();
+const PORT = 3001;
+const SERVER_URI = `http://localhost:${PORT}`;
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+App.listen(PORT);
+App.get("/", (request, response) => {
+	response.redirect(`${SERVER_URI}/api`);
+});
+App.get("/api", (request, response) => {
+	response.send("<h1>Welcome to my API</h1>");
+});
+App.get("/api/persons", (request, response) => {
+    response.json(persons);
+});
+App.get("/info", (request, response) => {
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `)
+})
+
+console.log(`App starting at: http://localhost:${PORT}`);
