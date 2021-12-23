e153148629d26d90cfbe2318c8f0082a25d7961e
Anibal <androso421@gmail.com>
2021-12-23 15:01:29 -0600

    adding morgan

--- index.js
+++ index.js
@@ -3,34 +3,52 @@ const res = require("express/lib/response");
 const App = express();
 const PORT = 3001;
 const SERVER_URI = `http://localhost:${PORT}`;
+const morgan = require('morgan');
+const crypto = require("crypto");
+const requestLogger = (request, response, next) => {
+	console.log("Method:", request.method);
+	console.log("Path:  ", request.path);
+	console.log("Body:  ", request.body);
+	console.log("---");
+	next();
+};
 
-const crypto = require('crypto');
-
+morgan.token('content', (request, response) => {
+    return `${request.body}`;
+});
 
 App.use(express.json());
-let persons = [
-    { 
-      "id": crypto.randomUUID(),
-      "name": "Arto Hellas", 
-      "phoneNumber": "040-123456"
-    },
-    { 
-      "id": crypto.randomUUID(),
-      "name": "Ada Lovelace", 
-      "phoneNumber": "39-44-5323523"
-    },
-    { 
-      "id": crypto.randomUUID(),
-      "name": "Dan Abramov", 
-      "phoneNumber": "12-43-234345"
-    },
-    { 
-      "id": crypto.randomUUID(),
-      "name": "Mary Poppendieck", 
-      "phoneNumber": "39-23-6423122"
-    }
-]
+//TODO We gotta change the tiny for Custom Arguments
+App.use(morgan('tiny'));
 
+
+let persons = [
+	{
+		id: crypto.randomUUID(),
+		name: "Arto Hellas",
+		phoneNumber: "040-123456",
+	},
+	{
+		id: crypto.randomUUID(),
+		name: "Ada Lovelace",
+		phoneNumber: "39-44-5323523",
+	},
+	{
+		id: crypto.randomUUID(),
+		name: "Dan Abramov",
+		phoneNumber: "12-43-234345",
+	},
+	{
+		id: crypto.randomUUID(),
+		name: "Mary Poppendieck",
+		phoneNumber: "39-23-6423122",
+	},
+    {
+		id: 10,
+		name: "Mary Sana",
+		phoneNumber: "39-6423122",
+	},
+];
 
 App.listen(PORT);
 App.get("/", (request, response) => {
@@ -40,52 +58,59 @@ App.get("/api", (request, response) => {
 	response.send("<h1>Welcome to my API</h1>");
 });
 App.get("/api/persons", (request, response) => {
-    response.json(persons);
+	response.json(persons);
 });
 App.get("/api/persons/:id", (request, response) => {
-    
-    const id = request.params.id;
-    
-    const person = persons.find(person => person.id === id);
+	const id = request.params.id;
 
-    if (person) {
-       return response.json(person);
-    }
-    
-    response.statusMessage = "That person is not in the phonebook";
-    response.status(404).end();
+	const person = persons.find((person) => person.id === id);
+
+	if (person) {
+		return response.json(person);
+	}
+
+	response.statusMessage = "That person is not in the phonebook";
+	response.status(404).end();
 });
 App.get("/info", (request, response) => {
-    response.send(`
+	response.send(`
         <p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date()}</p>
-    `)
+    `);
 });
 
 App.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
-    response.status(204).end();
-})
+	const id = Number(request.params.id);
+	persons = persons.filter((person) => person.id !== id);
+	response.status(204).end();
+});
 
-App.post('/api/persons', (request, response) => {
-    const person = request.body;
-    if (!person) {
-        return response.status(400).json({"error": "data is missing"});
-    } else if (!person.name || !person.phoneNumber) {
-        return response.status(400).json({"error": "data is not complete"});
-    }
-    const dataAlreadyExists = alreadyExists(person, persons);
+App.post("/api/persons", (request, response) => {
+	const person = request.body;
+	if (!person) {
+		return response.status(400).json({ error: "data is missing" });
+	} else if (!person.name || !person.phoneNumber) {
+		return response.status(400).json({ error: "data is not complete" });
+	}
+	const dataAlreadyExists = alreadyExists(person, persons);
 
-    if (dataAlreadyExists === "name" || dataAlreadyExists === "phoneNumber") {
-        return response.status(400).json({"error": `${dataAlreadyExists} is already in the phonebook`});
-    }
+	if (dataAlreadyExists === "name" || dataAlreadyExists === "phoneNumber") {
+		return response
+			.status(400)
+			.json({ error: `${dataAlreadyExists} is already in the phonebook` });
+	}
 
-    person.id = crypto.randomUUID();
-    persons = persons.concat(person); 
-})
+	person.id = crypto.randomUUID();
+	persons = persons.concat(person);
+    response.status(200).end();
+});
 
 console.log(`App starting at: http://localhost:${PORT}`);
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'});
+}  
+App.use(unknownEndpoint);
 
 function alreadyExists(newPerson, persons) {
 	for (let person of persons) {
